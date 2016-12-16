6307c9587ca133576f386994d2ca2c728012c2f7
metalnerd97 <mustafa.shayan9@gmail.com>
2016-12-16 15:51:09 +0500

    Add files via upload
    weather function.

--- natlanv2.c
+++ natlanv2.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <windows.h>
 #define SIZE 512
 
 
@@ -9,6 +10,8 @@ void compile(int flag);
 void goTo();
 void currentTime();
 void date();
+void weather(char sent[]);
+void hostName();
 
 int main(){
 
@@ -16,21 +19,17 @@ int main(){
 	system("cls");
 	printf("\n\t------    WELCOME TO NATLAN     ------");
 	printf("\n\t- NATURAL LANGUAGE COMMAND INTERFACE -\n");
-	printf("\n\t(Type help or commands to bring up list of commands)");
-	printf("\n\tHello! I am Natlan. How may I assist you?\n");
+	printf("\n\tTo bring up commands, type \"commands\"");
+	printf("\n\tHello! I am Natlan. How may I assist you? (format: Natlan, ....)\n");
 	
 	char sent[100];
+	char text[100];
 	char natlanW[50] = "Natlan";
 
 	while(!(strstr(sent,"quit") !=NULL)){
 		printf("\n\tEnter command:\n\t-> ");
 		scanf ("%[^\n]%*c", sent);
-		while(sent[i]!='\0'){
-			if(sent[i]>64 && sent[i]<91){
-				sent[i]=sent[i]+32;
-			}
-			i++;
-		}
+		strcpy(text, sent);
 		if(strstr(sent,"compile")!=NULL){
 			if(strstr(sent, "gcc")!=NULL){
 				gccFlag = 1;
@@ -40,15 +39,22 @@ int main(){
 			goTo();
 		}else if((strstr(sent, "help")) || (strstr(sent, "commands")) !=NULL){
 			//helpMenu();
+			printf("showing commands");
 		}
-		else 
-			if((strstr(sent, "time")) !=NULL){
-				currentTime();
-			}
-			if((strstr(sent, "date")) || (strstr(sent, "day")) !=NULL){
-				date();
-			}		
-		else if(!(strstr(sent,"quit")||strstr(sent,"time") !=NULL)){
+		else if((strstr(sent, "time")) || (strstr(sent, "Time")) || (strstr(sent, "TIME")) !=NULL){
+			currentTime();
+		}
+		else if((strstr(sent, "date")) || (strstr(sent, "Date")) || (strstr(sent, "DATE")) || (strstr(sent, "Day")) || (strstr(sent, "day")) || (strstr(sent, "DAY")) || (strstr(sent, "Tareekh")) !=NULL){
+			date();
+		}
+		else if(strstr(sent, "weather") || (strstr(sent, "Weather")) || (strstr(sent, "WEATHER")) != NULL){
+			
+			weather(text);
+		}
+		else if(strstr(sent, "pc name")!=NULL){
+			hostName();
+		}		
+		else if(!(strstr(sent,"quit") !=NULL)){
 			printf("\n\tCommand unrecognized.\n");
 		}
 	}
@@ -87,6 +93,7 @@ void goTo(){
 	system(cCommand2);
 }
 
+/* current local time */
 void currentTime(){
 	char timedate[SIZE];
   	time_t curtime;
@@ -98,6 +105,7 @@ void currentTime(){
   	fputs (timedate, stdout);
 }
 
+/* local date */
 void date(){
 	char timedate[SIZE];
   	time_t curtime;
@@ -105,30 +113,34 @@ void date(){
 
   	curtime = time (NULL);
   	loctime = localtime (&curtime);
-  	strftime (timedate, SIZE, "\n\tToday is %A, %B %d, 2016.\n", loctime);
+  	strftime (timedate, SIZE, "\n\tToday is %A, %B %d, %Y.\n", loctime);
   	fputs (timedate, stdout);	
 }
-/*
-void googleSearch(){
-	char cCommand3[50];
-	char fName[50];
-	strcpy(cCommand2, "START http://www.google.com/");
-	printf("\n\tEnter the name of directory or web address: \n\t-> ");
-	fgets(fName, 50, stdin);
-	fflush(stdout);
-	strcat(cCommand2, fName);
-	printf("\n\tGoing to %s", fName);
-	system(cCommand2);
+
+/* pc name */
+void hostName(){
+	system("hostname");
 }
-*/
-/* void weather(){
+
+/* weather search */
+void weather(char sentText[]){
+	if(strstr(sentText, "London") || (strstr(sentText, "london")) || (strstr(sentText, "LONDON"))!=NULL)
+	{	
+		system("start http://www.bbc.com/weather/2643743");
+		printf("\n\tShowing weather in London (source: BBC Weather)\n");
+	}
 	
 }
-*/
+
+/* add numbers */
+/*void addNumbers(){
+	int add[512];
+	int i = 0;
+	for
+}*/
 
 /*
 void helpMenu(){
 	printf("\n\t------   NATLAN COMMAND MENU    ------");
 	printf("\n\t-compile: Compiles the file. Uses tcc as compiler by default unless you specify it to use gcc");
-
-}*/
+*/
